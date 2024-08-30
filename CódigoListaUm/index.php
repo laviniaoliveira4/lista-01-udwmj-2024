9d3c4c78e5b95a81062a8f04344b2955b9ea988f
Lavínia Oliveira <130006462+laviniaoliveira4@users.noreply.github.com>
2024-08-30 14:41:56 -0300

    Add files via upload

---
+++ CódigoListaUm/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $num1 = 5;
+    $num2 = 10;
+
+
+    $soma = $num1 + $num2;
+
+
+    echo "A soma de $num1 e $num2 é: $soma";
+
+?>
+// Este código define dois números, calcula a soma deles e exibe o resultado. 
